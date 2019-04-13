7d429f6dcf123e653dba7a936ae6827923a76abc
Jaymee Liu <{jaymeeliu@gmail.com}>
2019-04-13 16:15:16 -0700

    fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,42 +15,42 @@ const gameCard = [];
 
 // grab Gif from API
 const getGif = async () => {
-    event.preventDefault();
-    const giphyWord = inputField.value.trim().toLowerCase();
-    const urlToFetch = `${url}${giphyWord}${apiKey}`;
-    //console.log(urlToFetch)
-    try {
-        const response = await fetch(urlToFetch);
-        if (response.ok) {
-            console.log(response);
-            //code to excute with json response 
-            const jsonResponse = await response.json();
-            var results = jsonResponse.data;
-            console.log(jsonResponse)
-            console.log(results)
-            for (let i = 0; i < results.length; i++) {
-                const gifObj = {
-                    url: results[i].images.original.url,
-                    id: results[i].id,
-                    pts: 0
-                }
+  event.preventDefault();
+  const giphyWord = inputField.value.trim().toLowerCase();
+  const urlToFetch = `${url}${giphyWord}${apiKey}`;
+  //console.log(urlToFetch)
+  try {
+    const response = await fetch(urlToFetch);
+    if (response.ok) {
+      console.log(response);
+      //code to excute with json response 
+      const jsonResponse = await response.json();
+      var results = jsonResponse.data;
+      console.log(jsonResponse)
+      console.log(results)
+      for (let i = 0; i < results.length; i++) {
+        const gifObj = {
+          url: results[i].images.original.url,
+          id: results[i].id,
+          pts: 0
+        }
 
-                gameCard.push(gifObj)
+        gameCard.push(gifObj)
 
-                // console.log(gifObj);
-            }
+        // console.log(gifObj);
+      }
 
-        } else {
-            throw new Error("Request Failed!");
-        } //end of else
-    } // end of try
-    catch (error) {
-        console.log(error.message)
-    } //end of catch
+    } else {
+      throw new Error("Request Failed!");
+    } //end of else
+  } // end of try
+  catch (error) {
+    console.log(error.message)
+  } //end of catch
 
-    randomNumGenerator(gameCard);
-    makeCard(gameCard);
-    // console.log(gameCard);
+  randomNumGenerator(gameCard);
+  makeCard(gameCard);
+  // console.log(gameCard);
 } //end of grab gif 
 
 //listener 
@@ -58,19 +58,19 @@ submit.addEventListener("click", getGif);
 
 //random Num Generator
 const randomNumGenerator = (array) => {
-    for (i = 0; i < array.length; i++) {
-        array[i].pts = Math.floor(Math.random() * 12) + 1;
-        console.log(array[i].id + ": " + array[i].pts);
-    }
+  for (i = 0; i < array.length; i++) {
+    array[i].pts = Math.floor(Math.random() * 12) + 1;
+    console.log(array[i].id + ": " + array[i].pts);
+  }
 }
 
 //Make Game board - Step 2
 const gameBoard = () => {
-    document.querySelector(".computer-score").innerHTML = computerScore;
-    document.querySelector(".user-score").innerHTML = userScore;
-    document.querySelector(".win-score").innerHTML = wins;
-    document.querySelector(".loss-score").innerHTML = losses;
-    // console.log(computerScore)
+  document.querySelector(".computer-score").innerHTML = computerScore;
+  document.querySelector(".user-score").innerHTML = userScore;
+  document.querySelector(".win-score").innerHTML = wins;
+  document.querySelector(".loss-score").innerHTML = losses;
+  // console.log(computerScore)
 };
 
 
@@ -79,101 +79,85 @@ gameBoard();
 
 
 const makeCard = (array) => {
-    for (let i = 0; i < array.length; i++) {
-        const giphyDiv = document.createElement("div");
-        giphyDiv.classList.add("col-lg-3", "giphyDiv");
-        const giphyImage = document.createElement("img");
-        giphyImage.src = array[i].url;
-        giphyImage.setAttribute("data-id", array[i].id);
-        giphyImage.classList.add("giphyImage");
-        giphyDiv.appendChild(giphyImage);
-        document.getElementById("giphy-view").appendChild(giphyDiv);
-    } //end of forloop 
+  for (let i = 0; i < array.length; i++) {
+    const giphyDiv = document.createElement("div");
+    giphyDiv.classList.add("col-lg-3", "giphyDiv");
+    const giphyImage = document.createElement("img");
+    giphyImage.src = array[i].url;
+    giphyImage.setAttribute("data-id", array[i].id);
+    giphyImage.classList.add("giphyImage");
+    giphyDiv.appendChild(giphyImage);
+    document.getElementById("giphy-view").appendChild(giphyDiv);
+  } //end of forloop 
 
 } //end of makeCard
 
 
 const reset = () => {
-    userScore = 0;
-    computerScore = Math.floor(Math.random() * 102) + 19;
-    randomNumGenerator(gameCard);
-    document.querySelector(".computer-score").innerHTML = computerScore;
-    document.querySelector(".user-score").innerHTML = userScore;
+  userScore = 0;
+  computerScore = Math.floor(Math.random() * 102) + 19;
+  randomNumGenerator(gameCard);
+  document.querySelector(".computer-score").innerHTML = computerScore;
+  document.querySelector(".user-score").innerHTML = userScore;
 };
 
 
 //If a user wins
 const win = () => {
-    wins++;
-    alert("You win!");
-    document.querySelector(".win-score").innerHTML = wins;
-    reset();
+  wins++;
+  alert("You win!");
+  document.querySelector(".win-score").innerHTML = wins;
+  reset();
 
 };
 //If a user does not win
 const loss = () => {
-    losses++;
-    alert("You did not win, loser!");
-    document.querySelector(".loss-score").innerHTML = losses;
-    reset();
+  losses++;
+  alert("You did not win, loser!");
+  document.querySelector(".loss-score").innerHTML = losses;
+  reset();
 };
 
 
 //This makes the gifPic images clickabe and updaes the user score.
 const gamePlay = (array) => {
 
-//     console.log(array)
-// document.body.addEventListener('click', function (event, array) {
-//             if (event.srcElement.className == 'giphyImage'){
-// console.log(array)
-    //  document.addEventListener('click', function (e) {
-    //      if (e.target && e.target.id == 'data-id') {
-             //do something
+  $("body").on("click", ".giphyImage", function () {
 
+    console.log("clicked")
+    console.log(gameCard)
+    const idPic = this.getAttribute("data-id");
+    console.log(idPic);
+    console.log("clicked")
+    const foundPic = array.findIndex(gifPic => {
+      console.log(gifPic.id === idPic)
+      return gifPic.id === idPic
 
-    $("body").on("click", ".giphyImage", function () {
+    });
+    console.log(foundPic)
 
-    // window.getElementById("data-id").attachEvent("onclick",
-    //getElementById("data-id").addEventListener("click",function () {
-                // event.preventDefault()
-                console.log("clicked")
-            console.log(gameCard)
+    userScore = userScore + array[foundPic].pts
+    console.log(array[foundPic].pts)
+    console.log(foundPic)
 
-            // document.addEventListener("click",".giphyImage", function () {
+    document.querySelector(".user-score").innerHTML = userScore;
+    if (userScore == computerScore) {
+      win();
+    } else if (userScore > computerScore) {
+      loss();
+    }
+    console.log(userScore);
 
-            const idPic = this.getAttribute("data-id");
-            console.log(idPic);
-            console.log("clicked")
-            const foundPic = array.findIndex(gifPic => {
-                console.log(gifPic.id === idPic)
-                return gifPic.id === idPic
-
-            });
-            console.log(foundPic)
-
-            userScore = userScore + array[foundPic].pts
-            console.log(array[foundPic].pts)
-            console.log(foundPic)
-
-            document.querySelector(".user-score").innerHTML = userScore;
-            if (userScore == computerScore) {
-                win();
-            } else if (userScore > computerScore) {
-                loss();
-            }
-            console.log(userScore);
-
-        // })
-
-                 //}
-                 });
+  });
 
 
 }
 
 
 
+
+
+
+
 //play game
 gamePlay(gameCard);
-
-
